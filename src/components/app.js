8bb5b36d786f8c7bb6ce55fa97a93165c4b2c2a0
Irina <ire.80@mail.ru>
2021-07-11 17:11:12 +0300

    my commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import moment from "moment";
+
+import PortfolioContainer from './portfolio-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Irina Portfolio</h1>
+        <PortfolioContainer />
+        <div>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </div>
       </div>
     );
   }
